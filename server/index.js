7f8e418e6a63d5261e7aefc6c5b0018c4e209700
alexnikshith <cb.sc.u4cse23620@cb.students.amrita.edu>
2026-04-07 15:32:30 +0530

    Fix Express 5 wildcard PathError for Render deployment

--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ app.use('/api/upload', uploadRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('(.*)', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
   );
 } else {
